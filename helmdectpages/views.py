652293807ff054afde0c69bb2fc110a428f67ee0
John Erick Arzaga <arzagaerick11@gmail.com>
2023-11-27 22:52:01 +0800

    done task #2

--- helmdectpages/views.py
+++ helmdectpages/views.py
@@ -56,17 +56,79 @@ def home(request):
     return render(request, 'helmdectpages/home.html')
 
 def report_history(request):
-    return render(request, 'helmdectpages/report_history.html')
+    report_fields = {
+        "plate_number": "1234",
+        "helmet": "Yes",
+        "object_detected": "No",
+        "person_detected": "Yes"
+    }
 
+    for field, value in report_fields.items():
+        set_database_value(f"reports/{field}", value)
+
+    report_data = {
+        'plate_number': get_database_value("reports/plate_number"),
+        'helmet': get_database_value("reports/helmet"),
+        'object_detected': get_database_value("reports/object_detected"),
+        'person_detected': get_database_value("reports/person_detected"),
+    }
+
+    return render(request, 'helmdectpages/report_history.html', report_data)
 def data_visualization(request):
-    return render(request, 'helmdectpages/data_visualization.html')
+    report_fields = {
+        "date": "2023-11-27",
+        "time": "12:00",
+        "location": "Cavite",
+        "type_of_helmet": "Full Face"
+    }
+    
+    for field, value in report_fields.items():
+        set_database_value(f"reports/{field}", value)
+    
+    
+    report_data = {
+        'date': get_database_value("reports/date"),
+        'time': get_database_value("reports/time"),
+        'location': get_database_value("reports/location"),
+        'type_of_helmet': get_database_value("reports/type_of_helmet"),
+    }
+    
+    return render(request, 'helmdectpages/data_visualization.html', report_data)
 
 def detailed_reports(request):
-    return render(request, 'helmdectpages/detailed_reports.html')
+    report_fields = {
+        "number_of_motorcyclist_detected": "1",
+        "color": "Red",
+        "texture": "Smooth",
+        "proper_usage": "Yes",
+        "violations": "None"
+    }
+    
+    for field, value in report_fields.items():
+        set_database_value(f"detailed_reports/{field}", value)
+    
+    report_data = {
+        'motorcyclist_detected': get_database_value("detailed_reports/number_of_motorcyclist_detected"),
+        'plate_number': get_database_value("reports/plate_number"),
+        'location': get_database_value("reports/location"),
+        'time': get_database_value("reports/time"),
+        'type_of_helmet': get_database_value("reports/type_of_helmet"),
+        'color': get_database_value("detailed_reports/color"),
+        'texture': get_database_value("detailed_reports/texture"),
+        'proper_usage': get_database_value("detailed_reports/proper_usage"),
+        'violations': get_database_value("detailed_reports/violations")
+    }
+    
+    return render(request, 'helmdectpages/detailed_reports.html', report_data)
 
 def settings(request):
     return render(request, 'helmdectpages/settings.html')
 
+def set_database_value(path, value):
+    database.child(path).set(value)
+    
+def get_database_value(path):
+    return database.child(path).get().val()
 
 
 # def test_database():
