75a3225f3eedc60dc1053db17500089aa446d733
amiel-danao <21stfloordev@gmail.com>
2024-01-25 17:25:25 +0800

    replaced firebase project

--- helmdectpages/firebase_init.py
+++ helmdectpages/firebase_init.py
@@ -9,7 +9,7 @@ import json
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-cred = credentials.Certificate(os.path.join(BASE_DIR, 'helmdetect-firebase-adminsdk-utli9-4256deee14.json'))  # Replace with your service account key path
+cred = credentials.Certificate(os.path.join(BASE_DIR, 'helmetdetect2-firebase-adminsdk-r98rq-7b231713df.json'))  # Replace with your service account key path
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'helmdetect.appspot.com'  # Replace with your Firebase Storage bucket URL
 })
